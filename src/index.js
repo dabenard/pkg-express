0a0734896e31e463440eb8df43de8493dd2f938f
Damien Bénard <damien.benard@amplexor.com>
2021-05-31 18:30:59 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+
+// Express app
+const app = express();
+
+// Express views
+app.set('views', path.join(__dirname, '../public/views'));
+
+app.set('view engine', 'pug');
+
+app.use('*', (_req, res) => {
+  res.render('index.pug');
+});
+
+const listener = http.createServer(app).listen(8080, () => {
+  console.info('Server started, listening on %d', listener.address().port);
+});
