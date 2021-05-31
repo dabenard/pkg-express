7304c65aaf1aa069c9b5f06e444007f9c5c16001
Damien Bénard <damien.benard@amplexor.com>
2021-05-31 18:42:07 -0400

    Added comments

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ const express = require('express');
 
 // Express app
 const app = express();
-
 // Express views
 app.set('views', path.join(__dirname, '../public/views'));
-
+// Use pug as Express view engine
 app.set('view engine', 'pug');
-
+// Match all routes
 app.use('*', (_req, res) => {
   res.render('index.pug');
 });
 
+// Start HTTP server
 const listener = http.createServer(app).listen(8080, () => {
   console.info('Server started, listening on %d', listener.address().port);
 });
